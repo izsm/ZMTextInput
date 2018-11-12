a3b167544fc9e88ff30ca07c7f2e5c146ccc4d1d
张书孟 <zhang_shu_meng@163.com>
2018-11-12 11:13:31 +0800

    [commit]: ZMTextView添加占位字符

--- ZMTextInput/ZMTextView.swift
+++ ZMTextInput/ZMTextView.swift
@@ -9,15 +9,60 @@ import UIKit
 
 open class ZMTextView: UITextView {
 
+    /// 占位文字
+    open var placeholder: String = "" {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    /// 占位文字颜色
+    open var placeholderColor: UIColor = UIColor.gray {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
     /// 是否移除emoji 默认为 false
     open var isRemoveEmoji: Bool = false
+    
     /// 最大限制文本长度, 默认不限制长度
     open var maxLength: Int = LONG_MAX
+    
     /// 设定文本改变回调
     open var didValueChange: (UITextView) -> Void = { _ in }
+    
     /// 设定文本达到最大长度的回调
     open var didMaxLength: (UITextView) -> Void = { _ in }
 
+    public override init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+
+        // 成为第一响应者时注册通知监听文本变化
+        NotificationCenter.default.addObserver(self, selector: #selector(textViewDidChange(notification:)), name: UITextView.textDidChangeNotification, object: nil)
+    }
+    
+    open override func draw(_ rect: CGRect) {
+        guard !hasText else { return }
+        
+        var rect = rect
+        let attrs: [NSAttributedString.Key : Any] = [NSAttributedString.Key.font: font ?? UIFont.systemFont(ofSize: 12),
+                     NSAttributedString.Key.foregroundColor: placeholderColor]
+        rect.origin.x = 5
+        rect.origin.y = 8
+        rect.size.width -= 2 * rect.origin.x
+        placeholder.draw(in: rect, withAttributes: attrs)
+    }
+    
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        setNeedsDisplay()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     deinit {
         NotificationCenter.default.removeObserver(self)
     }
@@ -34,7 +79,7 @@ extension ZMTextView {
     
     override open func resignFirstResponder() -> Bool {
         let resign = super.resignFirstResponder()
-        // 注销第一响应者时移除文本变化的通知, 以免影响其它的`UITextField`对象.
+        // 注销第一响应者时移除文本变化的通知, 以免影响其它的`UITextView`对象.
         NotificationCenter.default.removeObserver(self, name: UITextView.textDidChangeNotification, object: nil)
         return resign
     }
@@ -42,7 +87,8 @@ extension ZMTextView {
 
 extension ZMTextView {
     @objc private func textViewDidChange(notification: Notification) {
-        
+        // 会重新调用drawRect:方法
+        setNeedsDisplay()
         guard let _ = notification.object as? ZMTextView,
             var texts = text,
             markedTextRange == nil else { return }
--- ZMTextInputDemo/ZMTextInputDemo/ViewController.swift
+++ ZMTextInputDemo/ZMTextInputDemo/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     }()
     
     private lazy var textField2: ZMTextField = {
-        let textField2 = ZMTextField(frame: CGRect(x: 50, y: 200, width: view.frame.size.width - 100, height: 50))
+        let textField2 = ZMTextField(frame: CGRect(x: 50, y: 160, width: view.frame.size.width - 100, height: 50))
         textField2.backgroundColor = UIColor.gray
         textField2.maxLength = 11
         textField2.isRemoveEmoji = true
@@ -27,10 +27,24 @@ class ViewController: UIViewController {
     }()
     
     private lazy var textView: ZMTextView = {
-        let textView = ZMTextView(frame: CGRect(x: 50, y: 300, width: view.frame.size.width - 100, height: 200))
+        let textView = ZMTextView(frame: CGRect(x: 50, y: 220, width: view.frame.size.width - 100, height: 200))
         textView.backgroundColor = UIColor.gray
         textView.maxLength = 11
         textView.isRemoveEmoji = true
+        textView.placeholder = "占位字符1"
+        textView.placeholderColor = UIColor.red
+        textView.textColor = UIColor.white
+        return textView
+    }()
+    
+    private lazy var textView2: ZMTextView = {
+        let textView = ZMTextView(frame: CGRect(x: 50, y: 430, width: view.frame.size.width - 100, height: 200))
+        textView.backgroundColor = UIColor.gray
+        textView.maxLength = 11
+        textView.isRemoveEmoji = true
+        textView.placeholder = "占位字符2"
+        textView.placeholderColor = UIColor.red
+        textView.textColor = UIColor.white
         return textView
     }()
     
@@ -40,6 +54,7 @@ class ViewController: UIViewController {
         view.addSubview(textField1)
         view.addSubview(textField2)
         view.addSubview(textView)
+        view.addSubview(textView2)
         
         textField1.didValueChange = { textField in
             debugPrint("textField1: \(textField.text ?? "")")
@@ -63,6 +78,14 @@ class ViewController: UIViewController {
         
         textView.didMaxLength = { textField in
             debugPrint("textView max")
+        }
+        
+        textView2.didValueChange = { textField in
+            debugPrint("textView2: \(textField.text ?? "")")
+        }
+        
+        textView2.didMaxLength = { textField in
+            debugPrint("textView2 max")
         }
         
     }
